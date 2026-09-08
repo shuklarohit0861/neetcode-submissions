d20b7494ab6d6c3ae2def96f342fbdf5c58944a4
Rohit shukla <shuklarohit086@gmail.com>
2026-09-08 09:57:52 +0530

    Add: binary-search - submission-0

---
+++ Data Structures & Algorithms/binary-search/submission-0.js
@@ -0,0 +1,25 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number}
+     */
+    search(nums, target) {
+        let res = -1;
+
+        let l = 0;
+        let r = nums.length -1;
+        while(l<=r){
+            let mid = Math.floor((r + l)/2);
+            if(nums[mid]=== target){
+                return mid;
+            }
+            if(nums[mid]< target){
+                l = mid+1;
+            }else {
+                r = mid - 1
+            }
+        }
+        return res;
+    }
+}
